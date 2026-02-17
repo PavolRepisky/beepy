8ff315e13f2063ba916d28f8333bfb4a7ba919c6
Pavol Repisky <pal.repisky@gmail.com>
2026-02-17 07:02:39 +0100

    feat: setup tRPC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
-const Page = () => {
-  return <div className="text-red-500">Hello world</div>;
+import { caller } from "@/trpc/server";
+
+const Page = async () => {
+  const users = await caller.getUsers();
+
+  return (
+    <div>
+      {users.map((user) => (
+        <div key={user.id}>{user.name}</div>
+      ))}
+    </div>
+  );
 };
 
 export default Page;
